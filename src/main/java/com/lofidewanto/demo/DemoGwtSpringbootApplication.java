9c026d96214f0c5c5410f9a2125948ec06194336
Lofi Dewanto <lofidewanto@gmail.com>
2016-04-11 17:01:55 +0200

    initial import

---
+++ src/main/java/com/lofidewanto/demo/DemoGwtSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.lofidewanto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGwtSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGwtSpringbootApplication.class, args);
+	}
+}
