db4ec27502080191340ce0f1c074a46c4c77f972
lofidewanto <lofidewanto@gmail.com>
2016-04-23 00:55:16 +0200

    add domain and service

---
+++ src/main/java/com/lofidewanto/demo/server/domain/AddressImpl.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.lofidewanto.demo.server.domain;
+
+import java.util.Collection;
+
+public class AddressImpl implements Address {
+
+	@Override
+	public Collection<String> getNewAddresses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStreet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStreet(String street) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Person getPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPerson(Person person) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public AddressType getAddressType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAddressType(AddressType addressType) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isOld() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setOld(boolean old) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
