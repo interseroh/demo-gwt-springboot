32e0bd9de9a69091649bcde4aeecc9f0fdb7e9c4
lofidewanto <lofidewanto@gmail.com>
2016-05-23 15:12:39 +0200

    add test with GwtMock

--- src/test/java/com/lofidewanto/demo/client/ui/main/MainPanelViewTest.java
+++ src/test/java/com/lofidewanto/demo/client/ui/main/MainPanelViewTest.java
@@ -18,30 +18,89 @@
  */
 package com.lofidewanto.demo.client.ui.main;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwtmockito.GwtMockitoTestRunner;
+import com.lofidewanto.demo.client.common.ErrorFormatter;
+import com.lofidewanto.demo.client.common.LoadingMessagePopupPanel;
+import com.lofidewanto.demo.client.common.WidgetName;
+import com.lofidewanto.demo.client.ui.person.PersonPanelView;
+
+@RunWith(GwtMockitoTestRunner.class)
 public class MainPanelViewTest {
+
+	private MainPanelView view;
+
+	@Mock
+	private ErrorFormatter errorFormatter;
+
+	@Mock
+	private LoadingMessagePopupPanel loadingMessagePopupPanel;
+
+	@Mock
+	private EventBus eventBus;
+
+	@Mock
+	private PersonPanelView personPanelView;
 
 	@Before
 	public void setUp() throws Exception {
+		view = new MainPanelView(eventBus, errorFormatter,
+				loadingMessagePopupPanel);
 	}
 
 	@Test
 	public void testAddWidget() {
-		fail("Not yet implemented");
+		// Prepare
+		doNothing().when(view.contentColumn).add(personPanelView);
+
+		// CUT
+		view.addWidget(WidgetName.PERSONLIST, personPanelView);
+
+		// Asserts
+		assertEquals(view.widgets.size(), 1);
+		verify(personPanelView, times(1)).setVisible(false);
+		verify(view.contentColumn, times(1)).add(personPanelView);
 	}
 
 	@Test
 	public void testShowWidget() {
-		fail("Not yet implemented");
+		// Prepare
+		view.addWidget(WidgetName.PERSONLIST, personPanelView);
+		doReturn(1).when(view.contentColumn).getWidgetCount();
+
+		// CUT
+		view.showWidget(WidgetName.PERSONLIST);
+
+		// Asserts
+		assertEquals(view.widgets.size(), 1);
+		verify(view.contentColumn, times(1)).getWidget(anyInt());
+		verify(personPanelView, times(1)).setVisible(false);
+		verify(personPanelView, times(1)).setVisible(true);
 	}
 
 	@Test
 	public void testUpdatePersonPanelView() {
-		fail("Not yet implemented");
+		// Prepare
+		view.addWidget(WidgetName.PERSONLIST, personPanelView);
+		doReturn(1).when(view.contentColumn).getWidgetCount();
+
+		// CUT
+		view.updatePersonPanelView();
+
+		// Asserts
+		assertEquals(view.widgets.size(), 1);
 	}
 
 }
