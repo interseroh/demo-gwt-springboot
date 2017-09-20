5f2bf4443298e3495205aff962306c3fbfa7ebf6
lofidewanto <lofidewanto@gmail.com>
2017-09-21 00:34:10 +0200

    add some JsInterop

--- src/main/resources/public/myfunction.js
+++ src/main/resources/public/myfunction.js
@@ -4,7 +4,5 @@
  * This file needs to be injected, so that the MyJavaScriptHello class
  * can be compiled before.
  */
-function myFunction() {
-    var myJavaScriptHello = new MyJavaScriptHello('Lofi');
-    console.log(myJavaScriptHello.click());
-}
+var myJavaScriptHello = new MyJavaScriptHello('Lofi');
+myJavaScriptHello.click();
